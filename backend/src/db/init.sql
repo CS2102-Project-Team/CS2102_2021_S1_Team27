b56ded836e9a167977d4ee5b263ff6b8c433f095
kylejyx <jiang@u.nus.edu>
2020-10-01 20:54:04 +0800

    add accounts, cards table creation

---
+++ backend/src/db/init.sql
@@ -0,0 +1,14 @@
+DROP IF EXISTS accounts;
+DROP IF EXISTS cards;
+
+CREATE TABLE accounts(
+    username VARCHAR PRIMARY KEY,
+    passwd VARCHAR,
+    email VARCHAR
+);
+CREATE TABLE cards(
+    cardnumber VARCHAR PRIMARY KEY,
+    CVV VARCHAR(4),
+    expdate DATA,
+    username VARCHAR REFERENCES accounts(username)
+);
