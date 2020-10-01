3be7324d2251a6a0fa16541a077b9082ed34a8b5
kylejyx <jiang@u.nus.edu>
2020-10-01 23:03:15 +0800

    fix format problems

--- backend/src/routers/auth.js
+++ backend/src/routers/auth.js
@@ -1,26 +1,26 @@
 const jwt = require('jsonwebtoken');
-/** */ 
-* Middleware for authenticating JWT tokens 
-* @param {Request} req - request 
-* @param {Response} res - response 
-* @param {RequestHandler} next - next middleware to be called // import type! 
+/*
+ Middleware for authenticating JWT tokens 
+ @param {Request} req - request 
+ @param {Response} res - response 
+ @param {RequestHandler} next - next middleware to be called // import type! 
 */
 
 function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    if (!token) {
-        res.sendStatus(401);
-        return;
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token) {
+    res.sendStatus(401);
+    return;
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      res.sendStatus(403);
+      return;
     }
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) {
-            res.sendStatus(403);
-            return;
-        }
-        req.user = user;
-        next();
-    });
+    req.user = user;
+    next();
+  });
 } 
 
 module.exports = {authenticateToken}
