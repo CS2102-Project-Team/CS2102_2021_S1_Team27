56a209b22c4af0e6011d587a094470db37e9a2f6
kylejyx <jiang@u.nus.edu>
2020-10-01 23:03:33 +0800

    add post /cards

--- backend/src/routers/userrouter.js
+++ backend/src/routers/userrouter.js
@@ -1,7 +1,14 @@
 const express = require('express');
-
+const auth = require('./auth');
+const db = require('../db/db');
 const router = express.Router();
 
 router.get('/', (req, res) => res.redirect(307, 'https://cs2102-doc.netlify.app/'));
+router.post('/cards', auth.authenticateToken, async (req, res, next) => {
+  const cardnumber = string(req.body.cardnumber);
+  const cvv = string(req.body.cvv);
+  const exp = string(req.body.exp);
+  result = await db.functions.insertCard(cardnumber, cvv, exp, username);
+});
 
 module.exports = router;
