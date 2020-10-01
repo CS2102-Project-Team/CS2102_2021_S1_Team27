30204eafee1f94df492622fc263feafb09a6f6e2
kylejyx <jiang@u.nus.edu>
2020-10-01 23:55:07 +0800

    update post /cards

--- backend/src/routers/userrouter.js
+++ backend/src/routers/userrouter.js
@@ -4,11 +4,31 @@ const db = require('../db/db');
 const router = express.Router();
 
 router.get('/', (req, res) => res.redirect(307, 'https://cs2102-doc.netlify.app/'));
+/*
 router.post('/cards', auth.authenticateToken, async (req, res, next) => {
   const cardnumber = string(req.body.cardnumber);
   const cvv = string(req.body.cvv);
   const exp = string(req.body.exp);
   result = await db.functions.insertCard(cardnumber, cvv, exp, username);
 });
-
+*/
+router.post('/cards', (req,res,next) => {req.user = 'kyle';next();}, async (req, res, next) => {
+    const cardnumber = req.body.cardnumber;
+    const cvv = req.body.cvv;
+    const exp = req.body.exp;
+    const username = req.user;
+    
+    if (typeof cardnumber !== 'string' || 
+        typeof cvv !== 'string' ||
+        typeof exp !== 'string'
+    ) {
+        res.status(400);
+    }
+    const insRes = await db.functions.insertCard(cardnumber, cvv, exp, username);
+    if (insRes.success) {
+        res.status(204).json('success');
+    } else {
+        res.status(500);
+    }
+  });
 module.exports = router;
