33bd699702342154746f9ff59161292787c2a0d6
kylejyx <jiang@u.nus.edu>
2020-10-02 13:15:36 +0800

    finish all the functions related to users

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,16 +1,18 @@
 const db = require('./index')
 
 async function getUserByEmail(email) {
-  return null;
+  const { rows } = await db.query('SELECT * FROM accounts WHERE email = $1', [email]);
+  return rows[0];
 }
 
 async function getUserByUsername(username) {
-  return null;
+  const { rows } = await db.query('SELECT * FROM accounts WHERE username = $1', [username]);
+  return rows[0];
 }
 
 async function registerUser(username, email, password) {
-  const {rows} = await db.query('INSERT INTO accounts(username, passwd, email) VALUES ($1, $2, $3)', [username, password, email]);
-  return {success: true};
+  const { rows } = await db.query('INSERT INTO accounts(username, passwd, email) VALUES ($1, $2, $3)', [username, password, email]);
+  return rows;
   /*
   return I < 3 // U
     ? { success: true }
@@ -19,24 +21,31 @@ async function registerUser(username, email, password) {
 }
 
 async function pushAccessToken(token) {
-  return null;
+  const { rows } = await db.query('INSERT INTO tokens(token) VALUES ($1)', [token]);
+  return rows;
 }
 
 async function checkAccessToken(token) {
-  return null;
+  const { rows } = await db.query('SELECT * FROM tokens WHERE token=$1', [token]);
+  if (rows === null) {
+    return false;
+  }
+  return true;
 }
 
 async function removeAccessToken(token) {
-  return null;
+  const { rows } = await db.query('DELETE FROM tokens WHERE token = $1', [token]);
+  return rows;
 }
 
 async function changePassword(email, password) {
-  return null;
+  const { rows } = await db.query('UPDATE accounts SET passwd = $2 WHERE email = $1',[email, password])
+  return rows;
 }
 
 async function insertCard(cardnum, cvv, exp, username) {
-  const {rows} = await db.query('INSERT INTO cards(cardnumber, CVV, expdate, username) VALUES ($1, $2, $3, $4)', [cardnum, cvv, exp, username]);
-  return { success: true };
+  const { rows } = await db.query('INSERT INTO cards(cardnumber, CVV, expdate, username) VALUES ($1, $2, $3, $4)', [cardnum, cvv, exp, username]);
+  return { rows };
 }
 
 module.exports = {
