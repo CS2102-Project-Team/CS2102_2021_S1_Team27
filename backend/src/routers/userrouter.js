93e66912aef87a00c98572428acfeb4ca694b822
kylejyx <jiang@u.nus.edu>
2020-10-02 10:42:12 +0800

    Put back authentication

--- backend/src/routers/userrouter.js
+++ backend/src/routers/userrouter.js
@@ -5,14 +5,10 @@ const router = express.Router();
 
 router.get('/', (req, res) => res.redirect(307, 'https://cs2102-doc.netlify.app/'));
 /*
-router.post('/cards', auth.authenticateToken, async (req, res, next) => {
-  const cardnumber = string(req.body.cardnumber);
-  const cvv = string(req.body.cvv);
-  const exp = string(req.body.exp);
-  result = await db.functions.insertCard(cardnumber, cvv, exp, username);
-});
+Skip authentication: (req,res,next) => {req.user = 'kyle';next();},
 */
-router.post('/cards', (req,res,next) => {req.user = 'kyle';next();}, async (req, res) => {
+
+router.post('/cards', auth.authenticateToken, async (req, res) => {
   const cardnumber = req.body.cardnumber;
   const cvv = req.body.cvv;
   const exp = req.body.exp;
