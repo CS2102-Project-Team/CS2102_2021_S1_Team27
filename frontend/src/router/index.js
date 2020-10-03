3222bb60d2b52d21792469fb0162154c464949dd
lirc572 <34601854+lirc572@users.noreply.github.com>
2020-10-03 03:51:35 +0000

    integrate vue-router, vuex, implement dummy login page

---
+++ frontend/src/router/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+Vue.use(Router);
+
+export const constantRoutes = [
+  {
+    path: '/login',
+    component: () => import('@/views/login/index'),
+    hidden: true,
+  },
+  {
+    path: '/404',
+    component: () => import('@/views/login/index'), // non-existent
+    hidden: true,
+  },
+  {
+    path: '/', // to be modified
+    component: () => import('@/views/login/index'), // non-existent
+    redirect: '/login',
+    children: [
+      {
+        path: 'dashboard',
+        component: () => import('@/views/login/index'),
+        name: 'Dashboard',
+        meta: { title: 'Dashboard', icon: 'dashboard', affix: true },
+      },
+    ],
+  },
+];
+
+const createRouter = () => new Router({
+  // mode: 'history', // require service support
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRoutes,
+});
+
+const router = createRouter();
+
+// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
+export function resetRouter() {
+  const newRouter = createRouter();
+  router.matcher = newRouter.matcher; // reset router
+}
+
+export default router;
---
+++ frontend/src/store/index.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-param-reassign */
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    count: 0,
+  },
+  mutations: {
+    increment(state) {
+      state.count += 1;
+    },
+  },
+});
+
+export default store;
