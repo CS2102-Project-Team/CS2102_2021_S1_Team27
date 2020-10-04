b7cde0e6b25de18c9e7fdf3838ed518a04faa8a4
kylejyx <jiang@u.nus.edu>
2020-10-04 22:43:26 +0800

    Reformat

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,4 +1,4 @@
-const db = require('./index')
+const db = require('./index');
 
 async function getUserByEmail(email) {
   const { rows } = await db.query('SELECT * FROM accounts WHERE email = $1', [email]);
@@ -39,7 +39,7 @@ async function removeAccessToken(token) {
 }
 
 async function changePassword(email, password) {
-  const { rows } = await db.query('UPDATE accounts SET passwd = $2 WHERE email = $1',[email, password])
+  const { rows } = await db.query('UPDATE accounts SET passwd = $2 WHERE email = $1', [email, password]);
   return rows;
 }
 
@@ -47,7 +47,6 @@ async function getCards(username) {
   const { rows } = await db.query('SELECT cardnumber FROM cards WHERE username = $1', [username]);
   return rows;
 }
-
 
 async function insertCard(cardnum, cvv, exp, username) {
   const { rows } = await db.query('INSERT INTO cards(cardnumber, CVV, expdate, username) VALUES ($1, $2, $3, $4)', [cardnum, cvv, exp, username]);
