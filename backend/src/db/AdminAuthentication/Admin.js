79781fd7ae79cf2638cd29c23dcb23a8712cb4bd
richardcom <leonardlym61@gmail.com>
2020-10-06 20:12:30 +0800

    Update Admin functions

--- backend/src/db/AdminAuthentication/Admin.js
+++ backend/src/db/AdminAuthentication/Admin.js
@@ -1,24 +1,14 @@
 const db = require('../index');
 
 
-async function getUserByEmail(email) {
-  const { rows } = await db.query('SELECT * FROM accounts WHERE email = $1', [email]);
+async function getAdminByEmail(email) {
+  const { rows } = await db.query('SELECT * FROM admins WHERE email = $1', [email]);
   return rows[0];
 }
 
-async function getUserByUsername(username) {
-  const { rows } = await db.query('SELECT * FROM accounts WHERE username = $1', [username]);
+async function getAdminByUsername(username) {
+  const { rows } = await db.query('SELECT * FROM admins WHERE username = $1', [username]);
   return rows[0];
-}
-
-async function registerUser(username, email, password) {
-  const { rows } = await db.query('INSERT INTO accounts(username, passwd, email) VALUES ($1, $2, $3)', [username, password, email]);
-  return rows;
-  /*
-  return I < 3 // U
-    ? { success: true }
-    : { success: false, error: 'Nah' };
-  */
 }
 
 async function pushAccessToken(token) {
@@ -40,16 +30,15 @@ async function removeAccessToken(token) {
 }
 
 async function changePassword(email, password) {
-  const { rows } = await db.query('UPDATE accounts SET passwd = $2 WHERE email = $1', [email, password]);
+  const { rows } = await db.query('UPDATE admins SET passwd = $2 WHERE email = $1', [email, password]);
   return rows;
 }
 
 
 module.exports = {
   functions: {
-    getUserByEmail,
-    getUserByUsername,
-    registerUser,
+    getAdminByEmail,
+    getAdminByUsername,
     pushAccessToken,
     checkAccessToken,
     removeAccessToken,
