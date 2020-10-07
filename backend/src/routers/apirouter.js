2da503f2f0c7691aa1a4774c7ac6bb3aa0219617
kylejyx <jiang@u.nus.edu>
2020-10-07 11:31:48 +0800

    add adminRouter

--- backend/src/routers/apirouter.js
+++ backend/src/routers/apirouter.js
@@ -4,10 +4,13 @@ const router = express.Router();
 
 const authRouter = require('./authrouter');
 const userRouter = require('./userrouter');
+const adminRouter = require('./adminrouter');
 
 router.use('/auth', authRouter);
 
 router.use('/user', userRouter);
+
+router.use('/admin', adminRouter);
 
 router.get('/', (req, res) => res.redirect(307, 'https://cs2102-doc.netlify.app/'));
 
