c5a3a216f57f1e64bda46ef43eb6f2515712dcc3
kylejyx <jiang@u.nus.edu>
2020-10-11 19:55:53 +0800

    GET&POST caretaker/, POST&PUT&DELETE petcategory

--- backend/src/db/ctaker.js
+++ backend/src/db/ctaker.js
@@ -1,13 +1,28 @@
 const db = require('./index');
 
-async function getUserByEmail(email) {
-  const { rows } = await db.query('SELECT * FROM accounts WHERE email = $1', [email]);
-  return rows[0];
+async function getCaretaker(username) {
+  const { rows } = await db.query('SELECT fulltime, (CASE WHEN numrating=0 THEN -1 ELSE (sumrating+0.0)/numrating END) AS rating FROM caretakers WHERE username = $1', [username]);
+  return rows;
 }
 
-async function getUserByUsername(username) {
-  const { rows } = await db.query('SELECT * FROM accounts WHERE username = $1', [username]);
-  return rows[0];
+async function insertCaretaker(username) {
+  const { rows } = await db.query('INSERT INTO caretakers(username, fulltime, maxpets) VALUES ($1, false, 2)', [username]);
+  return rows;
+}
+
+async function insertCategory(username, pettype, price) {
+  const { rows } = await db.query('INSERT INTO looksafter(ctaker, ptype, price) VALUES ($1, $2, $3)', [username, pettype, price]);
+  return rows;
+}
+
+async function updatePrice(username, pettype, price) {
+  const { rows } = await db.query('UPDATE looksafter SET price=$3 WHERE ctaker=$1 AND ptype=$2', [username, pettype, price]);
+  return rows;
+}
+
+async function deleteCategory(username, pettype) {
+  const { rows } = await db.query('DELETE FROM looksafter WHERE ctaker=$1 AND ptype=$2', [username, pettype]);
+  return rows;
 }
 
 async function registerUser(username, email, password) {
@@ -22,8 +37,11 @@ async function registerUser(username, email, password) {
 
 module.exports = {
   functions: {
-    getUserByEmail,
-    getUserByUsername,
+    getCaretaker,
+    insertCaretaker,
+    insertCategory,
+    updatePrice,
+    deleteCategory,
     registerUser,
   },
 };
--- backend/src/routers/ctakerrouter.js
+++ backend/src/routers/ctakerrouter.js
@@ -5,10 +5,19 @@ const db = require('../db/ctaker');
 
 const router = express.Router();
 
-router.get('/user', auth.authenticateToken, async (req, res) => {
+/* Unfinished */
+router.get('/', auth.authenticateToken, async (req, res) => {
   try {
-    const user = await db.functions.getUserByUsername(req.user.username);
-    res.status(200).json({ username: user.username, email: user.email });
+    const rows = await db.functions.getCaretaker(req.user.username);
+    if (rows.length === 0) {
+      res.status(521).json({ error: 'User is not registered as a care taker' });
+      return;
+    }
+    const results = {};
+    results.type = (rows[0].fulltime) ? 'full time' : 'part time';
+    results.rating = rows[0].rating;
+    /* add more stuff */
+    res.status(200).json(results);
     return;
   } catch (err) {
     res.status(500).json('error');
@@ -19,42 +28,51 @@ router.get('/', (req, res) => res.redirect(307, 'https://cs2102-doc.netlify.app/
 Skip authentication: (req,res,next) => {req.user = 'kyle';next();},
 */
 
-router.get('/cards', auth.authenticateToken, async (req, res) => {
+router.post('/', auth.authenticateToken, async (req, res) => {
   try {
-    const insRes = await db.functions.getCards(req.user.username);
-    res.status(200).json(insRes);
-    return;
-  } catch (err) {
-    res.status(500).json('error');
-  }
-});
-
-router.post('/cards', auth.authenticateToken, async (req, res) => {
-  const { cardnumber } = req.body;
-  const { cvv } = req.body;
-  const { exp } = req.body;
-  const { username } = req.user;
-
-  if (typeof cardnumber !== 'string'
-        || typeof cvv !== 'string'
-        || typeof exp !== 'string'
-  ) {
-    res.status(400);
-    return;
-  }
-  try {
-    await db.functions.insertCard(cardnumber, cvv, exp, username);
+    await db.functions.insertCaretaker(req.user.username);
     res.status(204).json('success');
     return;
   } catch (err) {
+    if (err.code === '23505') {
+      res.status(422).json({ error: 'already a caretaker' });
+      return;
+    }
     res.status(500).json('error');
   }
 });
 
-router.delete('/cards', auth.authenticateToken, async (req, res) => {
+router.post('/petcategory', auth.authenticateToken, async (req, res) => {
   try {
-    const { cardnumber } = req.body;
-    await db.functions.deleteCard(cardnumber);
+    await db.functions.insertCategory(req.user.username, req.body.pettype, req.body.price);
+    res.status(204).json('success');
+    return;
+  } catch (err) {
+    if (err.code === '23505') {
+      res.status(422).json({ error: 'already exists that category' });
+      return;
+    }
+    res.status(500).json('error');
+  }
+});
+
+router.put('/petcategory', auth.authenticateToken, async (req, res) => {
+  try {
+    await db.functions.updatePrice(req.user.username, req.body.pettype, req.body.price);
+    res.status(204).json('success');
+    return;
+  } catch (err) {
+    if (err.code === '23505') {
+      res.status(422).json({ error: 'does not exist that category' });
+      return;
+    }
+    res.status(500).json('error');
+  }
+});
+
+router.delete('/petcategory', auth.authenticateToken, async (req, res) => {
+  try {
+    await db.functions.deleteCategory(req.user.username, req.body.pettype);
     res.status(204).json('success');
     return;
   } catch (err) {
