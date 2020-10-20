7673520750b65ab3d515a811101e261838272adc
lirc572 <lirc572@gmail.com>
2020-10-21 00:37:24 +0800

    update navbar

--- frontend/src/components/navbar.vue
+++ frontend/src/components/navbar.vue
@@ -1,70 +1,103 @@
 <template>
-  <el-header>
-    <el-row>
-      <el-col :span="1" style="text-align: left;">
-        Logo
-      </el-col>
-      <el-col :span="15" style="text-align: left;">
-        Pet Anything
-      </el-col>
-      <el-col :span="8">
-        <el-menu
-          :default-active="activeIndex"
-          class="headermenu"
-          mode="horizontal"
-          @select="handleSelect">
-            <el-menu-item index="logout" class="ral">Log out</el-menu-item>
-            <el-menu-item index="/caretaker" class="ral">Care Taker</el-menu-item>
-            <el-menu-item index="/petowner" class="ral">Pet Owner</el-menu-item>
-            <el-menu-item index="/profile" class="ral">Profile</el-menu-item>
-        </el-menu>
-      </el-col>
-    </el-row>
-  </el-header>
+  <header>
+    <ul>
+      <li class="right" @click="logout">
+        <p>Log Out</p>
+      </li>
+      <li class="right" @click="linkClicked('/caretaker')">
+        <p :class="$route.path.split('/')[1]==='/caretaker'?'active':''">Care Taker</p>
+      </li>
+      <li class="right" @click="linkClicked('/petowner')">
+        <p :class="$route.path.split('/')[1]==='/po'?'active':''">Pet Owner</p>
+      </li>
+      <li class="left title" @click="linkClicked('/')">
+        <p class="title-text">Pet-Anything</p>
+      </li>
+      <li class="right" @click="linkClicked('/profile')">
+        <p :class="$route.path.split('/')[1]==='/profile'?'active':''">Profile</p>
+      </li>
+    </ul>
+  </header>
 </template>
 
 <script>
 
 export default {
   name: 'navbar',
-  data() {
-    return {
-      activeIndex: '0',
-    };
-  },
   methods: {
-    handleSelect(key) {
-      if (key.substr(0, 1) === '/') {
-        this.$router.push(key);
-      } else if (key === 'logout') {
-        this.$store.dispatch('logout')
-          .then(() => {
-            this.$router.push('/');
-          })
-          .catch(({ error }) => {
-            // eslint-disable-next-line no-console
-            console.log(error);
-          });
+    linkClicked(route) {
+      if (route !== this.$router.currentRoute.path) {
+        if (this.$router.currentRoute.path === '/profile') {
+          if (route === '/') {
+            return;
+          }
+        }
+        this.$router.push(route);
       }
+    },
+    logout() {
+      this.$store.dispatch('logout')
+        .then(() => {
+          this.$router.push('/');
+        })
+        .catch(({ error }) => {
+          // eslint-disable-next-line no-console
+          console.log(error);
+        });
     },
   },
 };
 </script>
 
-<style>
-  .el-header {
-    background-color: #B3C0D1;
-    color: #333;
+<style scoped>
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #AAB7B8;
+  }
+
+  .title-text {
+    position: relative;
+    top: -2px;
+    font-family: 'Kaushan Script', cursive;
+    color: slateblue;
+    font-weight: bold;
+  }
+
+  li {
+    position: relative;
+    top: 1px;
+    cursor: pointer;
+  }
+
+  li p {
+    display: block;
+    color: #5B2C6F;
     text-align: center;
-    line-height: 60px;
+    padding: 16px 16px;
+    text-decoration: none;
+    font-family: 'Courgette', cursive;
   }
 
-  .headermenu {
-    background-color: #B3C0D1;
+  li p:hover:not(.active):not(.title-text) {
+    background-color: wheat;
   }
 
-  .ral {
-    float: right !important;
+  .active {
+    background-color: sandybrown;
   }
 
+  .title {
+    padding-left: 1%;
+  }
+
+  .left {
+    float: left;
+  }
+
+  .right {
+    float: right;
+  }
 </style>
--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -3,14 +3,9 @@ import beforeLogIn from './before-login';
 
 export default [
   {
-    path: '/profile',
-    component: () => import('@/views/layout/layout'),
-    children: mainRoutes,
-  },
-  {
     path: '/',
-    component: () => import('@/views/layout/beforeLogInLayout'),
-    children: beforeLogIn,
+    component: () => import('@/views/layout/layout'),
+    children: beforeLogIn.concat(mainRoutes),
   },
   {
     name: '404',
