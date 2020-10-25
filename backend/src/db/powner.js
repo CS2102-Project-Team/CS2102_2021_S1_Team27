f0a8e0beeadbb18a1a5df4b90e3b5113d45cb8ca
kylejyx <65062602+kylejyx@users.noreply.github.com>
2020-10-25 20:36:43 +0800

    Add username in getService

--- backend/src/db/powner.js
+++ backend/src/db/powner.js
@@ -31,7 +31,7 @@ async function deletePet(username, petname) {
 }
 
 async function getService(petcategory, startdate, enddate) {
-  const { rows } = await db.query('SELECT realname, addres, fulltime, (CASE WHEN numrating=0 THEN -1 ELSE (sumrating+0.0)/numrating END) AS rating, price * ($3::DATE - $2::DATE + 1) AS totalprice FROM (caretakers C JOIN looksafter ON ctaker=C.username AND ptype=$1) NATURAL JOIN accounts WHERE ($3::DATE-$2::DATE+1) = (SELECT COUNT(*) FROM available A WHERE A.ctaker=C.username AND date>=$2 AND date<=$3 AND status=\'available\')', [petcategory, startdate, enddate]);
+  const { rows } = await db.query('SELECT username, realname, addres, fulltime, (CASE WHEN numrating=0 THEN -1 ELSE (sumrating+0.0)/numrating END) AS rating, price * ($3::DATE - $2::DATE + 1) AS totalprice FROM (caretakers C JOIN looksafter ON ctaker=C.username AND ptype=$1) NATURAL JOIN accounts WHERE ($3::DATE-$2::DATE+1) = (SELECT COUNT(*) FROM available A WHERE A.ctaker=C.username AND date>=$2 AND date<=$3 AND status=\'available\')', [petcategory, startdate, enddate]);
   return rows;
 }
 // just a little unsure -> is the address ctaker's or user's
