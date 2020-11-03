4555e5c9303dd7bb6db07ec0a2b330c02e25a146
richardcom <leonardyml61@gmail.com>
2020-11-03 19:46:50 +0800

    Minor fix

--- backend/src/routers/adminrouter.js
+++ backend/src/routers/adminrouter.js
@@ -230,7 +230,7 @@ router.get('/service', auth.authenticateAdminToken, async (req, res) => {
   }
 });
 
-router.get('/revenue', auth.authenticateToken, async (req, res) => {
+router.get('/revenue', auth.authenticateAdminToken, async (req, res) => {
   try {
     const { from } = req.query;
     const { to } = req.query;
